7594fe36eeaf7f80a29720c6ad176b9b05c35418
unknown <joshuadiasbf@gmail.com>
2024-11-28 20:30:11 -0300

    ATT pom.xml e Game entity

--- src/main/java/com/taskmanager/Task/model/Game.java
+++ src/main/java/com/taskmanager/Task/model/Game.java
@@ -3,11 +3,15 @@ package com.taskmanager.Task.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 
 
 @Getter
@@ -23,6 +27,9 @@ public class Game {
     private String name;
     private String description;
     private String image;
+
+   @Min(0)
+   @Max(100)
     private Integer progress;
     private String status;
     private String earnings;
